aa43e31dfc368a9faa7d1801a59d5f6d1b4e230e
deborah-p <deborah.powers@cgi.com>
2021-03-31 19:22:29 +0200

    boulot 03/31

---
+++ exemples/model-css/action.js
@@ -0,0 +1,19 @@
+angular.module ('cssApp', ['ui.router']);
+// le routage
+angular.module ('cssApp').config (function ($stateProvider, $urlRouterProvider, $locationProvider){
+	$stateProvider.state ({ name: 'grid', url: '/grid', templateUrl: 'page-grid.html' });
+	$stateProvider.state ({ name: 'flex', url: '/flex-box', templateUrl: 'page-flex.html' });
+	$stateProvider.state ({ name: 'form', url: '/formulaire', templateUrl: 'page-form.html' });
+	$stateProvider.state ({ name: 'bord', url: '/bordure', templateUrl: 'page-bord.html' });
+	$stateProvider.state ({ name: 'back', url: '/fond', templateUrl: 'page-back.html' });
+	$stateProvider.state ({ name: 'table', url: '/tableau', templateUrl: 'page-table.html' });
+	$stateProvider.state ({ name: 'list', url: '/liste', templateUrl: 'page-list.html' });
+	$stateProvider.state ({ name: 'math', url: '/math-ml', templateUrl: 'page-math.html' });
+	$stateProvider.state ({ name: 'space', url: '/espacement', templateUrl: 'page-space.html' });
+	$stateProvider.state ({ name: 'image', url: '/image-et-lien', templateUrl: 'page-image.html' });
+	$stateProvider.state ({ name: 'query', url: '/media-query', templateUrl: 'page-query.html' });
+	$stateProvider.state ({ name: 'shape', url: '/forme', templateUrl: 'page-shape.html' });
+	$stateProvider.state ({ name: 'anim', url: '/animation', templateUrl: 'page-anim.html' });
+	$urlRouterProvider.otherwise ('/forme');
+	$locationProvider.hashPrefix ('');
+});
