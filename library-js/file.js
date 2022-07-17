09dd83da724c23adb4d9d5a23ebd08e9b57004d0
deborah-powers <deborah.powers.dev@gmail.com>
2022-07-17 12:27:49 +0200

    maison 07/17 12:26

--- library-js/file.js
+++ library-js/file.js
@@ -1,10 +1,26 @@
-/* dépendence: text.js
-les fichiers
+/* fonctions pour ouvrir des fichiers
+dépendence: text.js
 */
 const extentionsImg =[ 'bmp', 'svg', 'jpg', 'jpeg', 'png', 'gif' ];
 const extentions =[ 'js', 'py', 'php', 'java', 'sql', 'css', 'txt', 'html', 'htm', 'xml', 'json', 'csv', 'tsv', 'mp3', 'mp4' ]
 	.concat (extentionsImg);
 
+// variables à modifier selon votre ordinateur
+const sep = '/';
+const pathRoot = '/home/deborah' + sep;
+const pathDesktop = pathRoot + 'Bureau' + sep;
+const pathArticles = pathDesktop + 'articles' + sep;
+const pathShortcut ={
+	'r/': pathRoot,
+	'b/': pathDesktop,
+	'a/': pathArticles,
+};
+String.prototype.shortcut = function(){
+	if (this.contain (pathRoot)) return this;
+	var start = this.slice (0,2);
+	for (var shortcut in pathShortcut) if (start == shortcut) start = this.replace (shortcut, pathShortcut[shortcut]);
+	return start;
+}
 function fromFile (fileName, callback){
 	var xhttp = new XMLHttpRequest();
 	if (callback){
