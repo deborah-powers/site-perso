b2158fd642a09ca9ab69292772a913a84462663f
deborah-powers <deborah.powers.dev@gmail.com>
2025-07-18 15:05:23 +0200

    maison 07/18 15:05

--- library-js/pave-3d.js
+++ library-js/pave-3d.js
@@ -23,9 +23,9 @@ pave-3d { --depth: 3cm; }
 	<p>front</p>
 </pave-3d>
 */
-/* conversion unité vers pixel
-	1cm = 37.7952756 px
-*/
+
+/* ------------ les fonctions utilitaires ------------ */
+
 const uniTtoPx ={
 	em: parseFloat (getComputedStyle (document.documentElement, null).fontSize),
 	cm: 37.7952756 * window.devicePixelRatio, mm: 377.952756 * window.devicePixelRatio,
@@ -54,7 +54,14 @@ String.prototype.fromPx = function(){
 	var nb = parseInt (nbStr);
 	return nb;
 }
-HTMLElement.prototype.createFace = function (background, border){
+HTMLElement.prototype.styleShape = function(){
+	this.style.display = 'block';
+	this.style.padding = '0';
+	this.style.textAlign = 'center';
+	this.style.transformStyle = 'preserve-3d';
+	this.style.backgroundPosition = 'center';
+}
+HTMLElement.prototype.styleFace = function (background, border){
 	this.style.margin = '0';
 	this.style.textAlign = 'center';
 	this.style.position = 'absolute';
@@ -69,16 +76,18 @@ HTMLElement.prototype.createFace = function (background, border){
 	this.style.backgroundSize = 'contain';
 	this.style.border = border;
 }
-HTMLElement.prototype.createSide = function (width, ecartLeft, depth, angle, background, border){
-	this.createFace (background, border);
+HTMLElement.prototype.styleSide = function (width, ecartLeft, depth, angle, background, border){
+	this.styleFace (background, border);
 	this.style.width = width + 'px';
 	this.style.left = ecartLeft + 'px';
 	this.style.transform = 'rotateY(' + angle + 'deg) translateZ(' + depth + 'px)';
 }
+/* ------------ les portions de formes ------------ */
+
 class Shape3d extends HTMLElement{
-	constructor (nbFaces){
+	constructor (sideNb){
 		super();
-		this.nbFaces = nbFaces;
+		this.sideNb = sideNb;
 		// mise en forme
 		this.vraiStyle = null;
 		this.background ="";
@@ -86,15 +95,10 @@ class Shape3d extends HTMLElement{
 		this.width =0
 		this.height =0
 		this.depth =0;
-		this.side =0;
+		this.sideWidth =0;
 	}
 	connectedCallback(){
-		// style du cube
-		this.style.display = 'block';
-		this.style.padding = '0';
-		this.style.textAlign = 'center';
-		this.style.transformStyle = 'preserve-3d';
-		this.style.backgroundPosition = 'center';
+		this.styleShape();
 		// infos pour styler les enfants
 		this.vraiStyle = getComputedStyle (this);
 		this.width = this.vraiStyle.width.fromPx();
@@ -102,7 +106,6 @@ class Shape3d extends HTMLElement{
 		this.setDepth();
 		this.background = this.vraiStyle.background;
 		this.border = this.vraiStyle.border;
-		this.style.background = 'none';
 		this.style.border = 'none';
 
 		// styler les enfants et corriger leur nombre
@@ -110,51 +113,106 @@ class Shape3d extends HTMLElement{
 		var nbChildren =0;
 		var observer = new MutationObserver (function (mutations){
 			nbChildren = nbChildren + mutationNb (mutations);
-			if (nbChildren < self.nbFaces){
-				self.appendChild (document.createElement ('p'));
-			}
+			if (nbChildren < self.sideNb +2) self.appendChild (document.createElement ('p'));
 			else{
-				console.log (self.width, self.height, self.depth);
 				self.setSide();
-				self.createHat();
-				self.createSides();
+				self.styleHat();
+				self.styleSides();
 		}});
 		observer.observe (this, { childList: true });
+		this.style.background = 'none';
 	}
 	// modifier selon la forme
 	setHeight(){ this.height = this.vraiStyle.height.fromPx(); }
-	setDepth(){ this.depth = this.vraiStyle.getPropertyValue ('--depth').coorDtoPx() /2.0; }
-	setSide(){ this.side = this.width; }
-	createHat(){
-		this.children[0].createFace (this.background, this.border);
+	setDepth(){ this.depth = this.width /2.0; }
+	setSide(){ this.sideWidth = this.width; }
+	styleHat(){
+		this.children[0].styleFace (this.background, this.border);
 		this.children[0].style.height = 2* this.depth + 'px';
 		this.children[0].style.transform = 'rotateX(90deg) translateZ(' + this.depth + 'px)';
 		this.children[0].id = 'top';
-		this.children[1].createFace (this.background, this.border);
+		this.children[1].styleFace (this.background, this.border);
 		this.children[1].style.height = 2* this.depth + 'px';
 		this.children[1].style.transform = 'rotateX(-90deg) translateZ(' + (this.height - this.depth) + 'px)';
 		this.children[1].id = 'bottom';
 	}
-	createSides(){
-		const angle = 360 / (this.nbFaces -2);
+	styleSides(){
+		const angle = 360 / this.sideNb;
 		var angleTmp =0;
-		const ecartLeft = (this.width - this.side) /2;
-		for (var c=2; c< this.nbFaces; c++){
-			this.children[c].createSide (this.side, ecartLeft, this.depth, angleTmp, this.background, this.border);
+		const ecartLeft = (this.width - this.sideWidth) /2;
+		for (var c=0; c< this.sideNb; c++){
+			this.children[c+2].styleSide (this.sideWidth, ecartLeft, this.depth, angleTmp, this.background, this.border);
 			angleTmp += angle;
 		}
 		/*/ adapter le sens des écritures selon le sens de rotation
-		if (this.nbFaces %2 ===0 && this.className.includes ('vertical')){
-			const idMilieu = this.nbFaces /2 +1;
+		if (this.sideNb %2 ===0 && this.className.includes ('vertical')){
+			const idMilieu = this.sideNb /2 +1;
 			this.children[idMilieu].style.transform = 'rotateX(' + angle * (idMilieu -2) + 'deg) translateZ(' + this.depth + 'px)';
 			this.classList.remove ('vertical');
 		}*/
 }}
+class ShapeQuart extends Shape3d{
+	connectedCallback(){
+		this.styleShape();
+		// infos pour styler les enfants
+		this.vraiStyle = getComputedStyle (this.parentElement);
+		this.width = this.vraiStyle.width.fromPx();
+		this.setHeight();
+		this.setDepth();
+		this.setSide();
+		this.background = this.vraiStyle.background;
+		this.border = 'none';
+		this.style.border = 'none';
+		// récupérer le style attribué à l'élément
+		var attributeObserver = new MutationObserver (function (mutations){
+			if ('style' === mutations[0].attributeName && 'none' !== mutations[0].target.style.background){
+				this.background = mutations[0].target.style.background;
+				mutations[0].target.style.background = 'none';
+		}});
+		attributeObserver.observe (this, { attributes: true });
+		this.styleSides();
+		this.style.background = 'none';
+	}
+//	setSide(){ this.sideWidth = this.width *0.25* Math.PI / this.sideNb; }
+	setSide(){ this.sideWidth = this.width *0.25* Math.PI /12; }
+	styleSides(){
+		for (var c=0; c< this.sideNb; c++) this.appendChild (document.createElement ('p'));
+		const rayon = this.width /2.0;
+		const ecartLeft = rayon - this.sideWidth /2;
+		const angle = 90 / this.sideNb;
+		var angleTmp = angle /2.0;
+		for (var c=0; c< this.sideNb; c++){
+			this.children[c].styleSide (this.sideWidth, ecartLeft, rayon, angleTmp, this.background, 'none');
+			angleTmp += angle;
+}}}
+class TubeQuart extends ShapeQuart{
+	constructor(){ super (12); }
+	connectedCallback(){
+		this.style.height = 'inherit';
+		super.connectedCallback();
+	}
+	styleSides(){
+		for (var c=0; c< this.sideNb; c++) this.appendChild (document.createElement ('p'));
+		const rayon = this.width /2.0;
+		const ecartLeft = rayon - this.sideWidth /2;
+		const angle = 90 / this.sideNb;
+		var angleTmp = angle /2.0;
+		for (var c=0; c<12; c++){
+			this.children[c].styleSide (this.sideWidth, ecartLeft, rayon, angleTmp, this.background, 'none');
+			angleTmp += angle;
+}}}
+class BoulQuart extends ShapeQuart{
+	constructor(){ super (36); }
+	connectedCallback(){
+		this.style.height = '33%';
+		super.connectedCallback();
+}}
+/* ------------ les formes complètes ------------ */
+
 class Pole extends Shape3d{
-	constructor(){ super (6); }
-	setDepth(){ this.depth = this.width /2.0; }
-	createSides(){
-		super.createSides();
+	constructor(){ super (4); }
+	styleSides(){
+		super.styleSides();
 		// adapter le sens des écritures selon le sens de rotation
 		if (this.className.includes ('vertical')){
 			this.children[4].style.transform = 'rotateX(180deg) translateZ(' + this.depth + 'px)';
@@ -162,8 +220,8 @@ class Pole extends Shape3d{
 }}}
 class Pave extends Pole{
 	setDepth(){ this.depth = this.vraiStyle.getPropertyValue ('--depth').coorDtoPx() /2.0; }
-	createSides(){
-		super.createSides();
+	styleSides(){
+		super.styleSides();
 		this.children[3].style.width = 2* this.depth + 'px';
 		this.children[3].style.transform = 'rotateY(90deg) translateZ(' + (this.width - this.depth) + 'px)';
 		this.children[5].style.width = 2* this.depth + 'px';
@@ -173,40 +231,40 @@ class Cube extends Pole{
 		this.height = this.width;
 		this.style.height = this.width + 'px';
 	}
-	createSides(){
-		super.createSides();
+	styleSides(){
+		super.styleSides();
 		this.children[2].style.height = this.width + 'px';
 		this.children[3].style.height = this.width + 'px';
 		this.children[4].style.height = this.width + 'px';
 		this.children[5].style.height = this.width + 'px';
 }}
 class Hexa extends Shape3d{
-	constructor(){ super (8); }
-	setDepth(){ this.depth = this.vraiStyle.width.fromPx() * 0.433; }
-	setSide(){ this.side = this.vraiStyle.width.fromPx() /2; }
-	createHat(){
-		super.createHat();
+	constructor(){ super (6); }
+	setDepth(){ this.depth = this.width * 0.433; }
+	setSide(){ this.sideWidth = this.width /2; }
+	styleHat(){
+		super.styleHat();
 		this.children[0].style.clipPath = 'polygon(25% 0, 75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%)';
 		this.children[1].style.clipPath = 'polygon(25% 0, 75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%)';
 }}
 class Tequi extends Shape3d{
-	constructor(){ super (5); }
-	setDepth(){ this.depth = this.vraiStyle.width.fromPx() * 0.433; }
-	createHat(){
-		super.createHat();
+	constructor(){ super (3); }
+	setDepth(){ this.depth = this.width * 0.433; }
+	styleHat(){
+		super.styleHat();
 		this.children[0].style.clipPath = 'polygon(0 100%, 100% 100%, 50% 0)';
 		this.children[1].style.clipPath = 'polygon(0 0, 100% 0, 50% 100%)';
 	}
-	createSides(){
-		super.createSides();
+	styleSides(){
+		super.styleSides();
 		this.children[3].style.transform = 'rotateY(120deg) translateZ(' + this.depth /2 + 'px) translateX(-12.5%)';
 		this.children[4].style.transform = 'rotateY(240deg) translateZ(' + this.depth /2 + 'px) translateX(12.5%)';
 }}
 class Trect extends Tequi{
-	setDepth(){ this.depth = this.vraiStyle.width.fromPx() * 0.25; }
-	createSides(){
-		super.createSides();
-		const width = this.vraiStyle.width.fromPx() * -0.145;
+	setDepth(){ this.depth = this.width * 0.25; }
+	styleSides(){
+		super.styleSides();
+		const width = this.width * -0.145;
 		this.children[3].style.width = '71%';
 		this.children[3].style.transform = 'rotateY(-45deg)';
 		this.children[3].style.left = 1.6* this.depth + 'px';
@@ -215,53 +273,112 @@ class Trect extends Tequi{
 		this.children[4].style.left = -0.4* this.depth + 'px';
 }}
 class Octo extends Shape3d{
-	constructor(){ super (10); }
-	setSide(){ this.side = this.vraiStyle.width.fromPx() * 0.414213; }
-	createHat(){
-		super.createHat();
+	constructor(){ super (8); }
+	setSide(){ this.sideWidth = this.width * 0.414213; }
+	styleHat(){
+		super.styleHat();
 		this.children[0].style.clipPath = 'polygon(29.28935% 0, 70.71065% 0, 100% 29.28935%, 100% 70.71065%, 70.71065% 100%, 29.28935% 100%, 0 70.71065%, 0 29.28935%)';
 		this.children[1].style.clipPath = 'polygon(29.28935% 0, 70.71065% 0, 100% 29.28935%, 100% 70.71065%, 70.71065% 100%, 29.28935% 100%, 0 70.71065%, 0 29.28935%)';
 }}
 class Cylindre extends Shape3d{
-	constructor(){ super (50); }
-	createHat(){
-		super.createHat();
+	constructor(){ super (4); }
+	connectedCallback(){
+		this.styleShape();
+		// style des enfants
+		this.vraiStyle = getComputedStyle (this);
+		this.width = this.vraiStyle.width.fromPx();
+		this.setHeight();
+		this.setDepth();
+		this.background = this.vraiStyle.background;
+		this.border = this.vraiStyle.border;
+		// styler les enfants et corriger leur nombre
+		this.setSide();
+		this.style.border = 'none';
+		const self = this;
+		var nbChildren =0;
+		var observer = new MutationObserver (function (mutations){
+			nbChildren = nbChildren + mutationNb (mutations);
+			if (nbChildren <2) self.appendChild (document.createElement ('p'));
+			else if (nbChildren <6) self.appendChild (document.createElement ('tube-quart'));
+			else{
+				self.styleHat();
+				self.styleSides();
+				self.style.background = 'none';
+		}});
+		observer.observe (this, { childList: true });
+	}
+	styleHat(){
+		super.styleHat();
 		this.children[0].style.borderRadius = '50%';
 		this.children[1].style.borderRadius = '50%';
 	}
-	setSide(){ this.side = this.vraiStyle.width.fromPx() * 0.065; }
-	createSides(){
-		const angle = 360 / (this.nbFaces -2);
+	styleSides(){
+		this.children[3].style.transform = 'rotateY(90deg) translateY(-100%)';
+		this.children[4].style.transform = 'rotateY(180deg) translateY(-200%)';
+		this.children[5].style.transform = 'rotateY(270deg) translateY(-300%)';
+	}
+}
+class Cylindre_va extends Shape3d{
+	constructor(){ super (48); }
+	connectedCallback(){
+		if (this.getAttribute ('sides')) this.sideNb = parseInt (this.getAttribute ('sides'));
+		super.connectedCallback();
+	}
+	styleHat(){
+		super.styleHat();
+		this.children[0].style.borderRadius = '50%';
+		this.children[1].style.borderRadius = '50%';
+	}
+	setSide(){ this.sideWidth = this.width * Math.PI / this.sideNb; }
+	styleSides(){
+		const angle = 360 / this.sideNb;
 		var angleTmp =0;
-		const ecartLeft = (this.vraiStyle.width.fromPx() - this.side) /2;
-		for (var c=2; c< this.nbFaces; c++){
-			this.children[c].createSide (this.side, ecartLeft, this.depth, angleTmp, this.background, this.border);
-			this.children[c].style.borderLeft = 'none';
-			this.children[c].style.borderRight = 'none';
+		const ecartLeft = (this.width - this.sideWidth) /2;
+		for (var c=0; c< this.sideNb; c++){
+			this.children[c+2].styleSide (this.sideWidth, ecartLeft, this.depth, angleTmp, this.background, this.border);
+			this.children[c+2].style.borderLeft = 'none';
+			this.children[c+2].style.borderRight = 'none';
 			angleTmp += angle;
 }}}
+Number.prototype.computeAngleForBallFacet = function(){ return Math.PI * this / (180.0 *8); }
 class Boule extends Shape3d{
-	constructor(){ super (98); }
-	createHat(){
-		super.createHat();
-		this.children[0].style.width = '10%';
-		this.children[0].style.borderRadius = '50%';
-		this.children[1].style.width = '10%';
-		this.children[1].style.borderRadius = '50%';
+	constructor(){ super (0); }
+	connectedCallback(){
+		this.styleShape();
+		// style des enfants
+		this.vraiStyle = getComputedStyle (this);
+		this.width = this.vraiStyle.width.fromPx();
+		this.setHeight();
+		this.setDepth();
+		this.background = 'none';
+		this.border = 'none';
+		// styler les enfants et corriger leur nombre
+		this.setSide();
+		this.styleSides();
+		this.style.border = 'none';
 	}
-	setDepth(){ this.depth = this.vraiStyle.width.fromPx() /2; }
-	setSide(){ this.side = this.vraiStyle.width.fromPx() * 0.065; }
-	createSides(){
-		const angle = 360 / (this.nbFaces -2);
-		var angleTmp =0;
-		const ecartLeft = (this.vraiStyle.width.fromPx() - this.side) /2;
-		for (var c=2; c< this.nbFaces; c++){
-			this.children[c].createSide (this.side, ecartLeft, this.depth, angleTmp, this.background, this.border);
-			this.children[c].style.borderLeft = 'none';
-			this.children[c].style.borderRight = 'none';
-			angleTmp += angle;
-}}}
+	setHeight(){
+		this.height = this.width;
+		this.style.height = this.width + 'px';
+	}
+	styleHat(){
+		this.appendChild (document.createElement ('p'));
+		this.appendChild (document.createElement ('p'));
+		const ecartLeft = (this.width - this.sideWidth) /2;
+		this.children[0].style.width = '10%';
+		this.children[0].style.height = '10%';
+		this.children[0].style.transform = 'rotateX(90deg) translateZ(' + this.depth /10.0 + 'px)';
+		this.children[0].style.left = ecartLeft + 'px';
+		this.children[1].style.width = '10%';
+		this.children[1].style.height = '10%';
+		this.children[1].style.transform = 'rotateX(-90deg) translateZ(' + (this.height - this.depth /10.0) + 'px)';
+		this.children[1].style.left = ecartLeft + 'px';
+	}
+	styleSides(){
+		this.appendChild (document.createElement ('boul-quart'));
+}}
 customElements.define ('pole-3d', Pole);
+customElements.define ('cube-3d', Cube);
 customElements.define ('hexa-3d', Hexa);
 customElements.define ('equi-3d', Tequi);
 customElements.define ('trec-3d', Trect);
@@ -269,4 +386,6 @@ customElements.define ('octo-3d', Octo);
 customElements.define ('pave-3d', Pave);
 customElements.define ('cyld-3d', Cylindre);
 customElements.define ('boul-3d', Boule);
-customElements.define ('cube-3d', Cube);
+customElements.define ('tube-quart', TubeQuart);
+customElements.define ('boul-quart', BoulQuart);
+
